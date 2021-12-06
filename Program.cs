cc42b10b41c86dd4c0d87fd3a8fbfa3dbf79cf0f
Jan-Erik Friedrich <oerk@outlook.com>
2021-12-06 12:56:34 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AoC21_1_Sonar_Sweep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
